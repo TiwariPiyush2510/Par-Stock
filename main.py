3393f6f4757168ec68f81544012082bae17e0fb9
TiwariPiyush2510 <piyushtiwari.pt@gmail.com>
2025-07-12 17:10:33 +0400

    Fix: CORS origin set to Netlify URL

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://preeminent-choux-a8ea17.netlify.app"],
+    allow_origins=["https://preeminent-choux-a8ea17.netlify.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
